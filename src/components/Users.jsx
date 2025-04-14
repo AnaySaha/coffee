57dd223cd93c7a82dc17e1dfab1e415af2c97895
Anay Saha <anaysahapaban@gmail.com>
2025-04-15 02:55:24 +0600

    chap-5

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,63 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 
 const Users = () => {
 
     const loadedUsers = useLoaderData();
     const [users, setUsers] = useState(loadedUsers);
+
+    const handleUserDelete = id => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+               
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                }); 
+                  // delete from the database
+             fetch(`http://localhost:5000/users/${id}`, {
+                methode: 'DELETE'
+             })
+             .then(res = res.json())
+             .then(data => {
+                if(data.deletedCount){
+                    swalWithBootstrapButtons.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+
+                    const remainingusers = users.filter(user => user._id !== id);
+                    setUsers(remainingusers);
+                    }
+
+             })
+            }
+           
+        });
+    }
     return (
         <div>
             <h2 className="text-3xl">Users: {users.length}</h2>
@@ -23,19 +75,23 @@ const Users = () => {
                     </thead>
                     <tbody>
                         {/* row 1 */}
-                    {
-                      users.map(user =>     <tr key={user._id}>
-                        <th>1</th>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.createdAt}</td>
-                        <td>
-                        <button className='btn'>X</button>
-                        </td>
-                    </tr>)  
-                    }
-                  
-                       
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>1</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.createdAt}</td>
+                                <td>
+                                    <button
+                                        onClick={() => handleUserDelete(
+                                            user.id
+                                        )}
+                                        className='btn'>X</button>
+                                </td>
+                            </tr>)
+                        }
+
+
                     </tbody>
                 </table>
             </div>
